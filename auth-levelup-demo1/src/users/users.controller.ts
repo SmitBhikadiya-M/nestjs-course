b8d421b2a3013047fd1b4bf9e022d00ecee05715
Smit Bhikadiya <smit.bhikadiya@marutitech.com>
2022-08-04 16:58:32 +0530

    Create Custom Roles Guard to protect some routes

--- auth-levelup-demo1/src/users/users.controller.ts
+++ auth-levelup-demo1/src/users/users.controller.ts
@@ -1,13 +1,16 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, SetMetadata } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Role } from './entities/role.enum';
+import { Roles } from './roles.decorator';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @Roles(Role.ADMIN)
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
@@ -22,11 +25,13 @@ export class UsersController {
     return this.usersService.findOne(+id);
   }
 
+  @Roles(Role.ADMIN)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
+  @Roles(Role.ADMIN)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
