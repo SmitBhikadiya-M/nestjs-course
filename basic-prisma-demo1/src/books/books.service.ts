87966f726872707047dbd071fc1c8c15a2c4bbd6
Smit Bhikadiya <smit.bhikadiya@marutitech.com>
2022-08-04 09:55:30 +0530

    Prisma Setup with CURD Operation Implementation

---
+++ basic-prisma-demo1/src/books/books.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
+import { UpdateBookDto } from './dto/update-book.dto';
+
+@Injectable()
+export class BooksService {
+
+  constructor(private prisma: PrismaService ) {}
+
+  create(createBookDto: Prisma.BooksCreateInput) {
+    return this.prisma.books.create({ data: createBookDto });
+  }
+
+  findAll() {
+    return this.prisma.books.findMany();
+  }
+
+  findOne(booksWhereUniqueInput: Prisma.BooksWhereUniqueInput) {
+    return this.prisma.books.findUnique({
+      where: booksWhereUniqueInput
+    });
+  }
+
+  update(where: Prisma.BooksWhereUniqueInput, data: Prisma.BooksUpdateInput) {
+    return this.prisma.books.update({
+      where,
+      data
+    });
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} book`;
+  }
+}
