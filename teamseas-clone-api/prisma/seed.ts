1dbeafe173c8ebfef8a8778b900cd61df4a2c868
Smit Bhikadiya <smit.bhikadiya@marutitech.com>
2022-08-08 12:52:22 +0530

    Added Prisma Confirguation and Some code for basic donation create and find by id or find all

---
+++ teamseas-clone-api/prisma/seed.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function main(){
+    await prisma.donation.deleteMany();
+    const alice = await prisma.donation.create({
+        data: {
+            email: 'alisce@prisma.io',
+            displayName: 'Alice',
+            count: 5
+        }
+    });
+    console.log(alice);
+    
+}
+
+main().catch((e)=>{
+    console.log(e);
+})
