510d5019cdfcf2d1fa7843d7fa9e0c289444345b
Smit Bhikadiya <smit.bhikadiya@marutitech.com>
2022-08-08 11:15:58 +0530

    Intial Confirugation For GraphQL

--- teamseas-clone-api/src/app.module.ts
+++ teamseas-clone-api/src/app.module.ts
@@ -1,9 +1,20 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DonationsModule } from './donations/donations.module';
+import { GraphQLDateTime } from 'graphql-iso-date';
 
 @Module({
-  imports: [],
+  imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver: ApolloDriver,
+    typePaths: ['./**/*.graphql'],
+    playground: false,
+    plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    resolvers: { DateTime: GraphQLDateTime },
+  }), DonationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
